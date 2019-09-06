a395c9be48dec78bd24b56f10ee4706de46f9494
Tarakeshwar Sriram <tsriram@ipass.com>
2019-09-05 22:24:06 -0700

    Update DeleteDuplicates.java
    delete duplicates from unorted list

--- src/com/himalaya/linkedlist/DeleteDuplicates.java
+++ src/com/himalaya/linkedlist/DeleteDuplicates.java
@@ -1,3 +1,4 @@
+import java.util.HashMap;
 /**
 * DeleteDupicates.java
 *
@@ -35,6 +36,26 @@ class DeleteDuplicates{
 		return (head.data == head.next.data)? head.next: head;
 	}
 
+
+	public static void deleteDuplicatesUnsortedList(Node n){
+
+		Node previous = null;
+		HashMap<Integer, Boolean> map = new HashMap<Integer, Boolean>();
+
+		while(n != null){
+
+			if(map.containsKey(n.data)){
+				previous.next = n.next;
+			}
+			else{
+				map.put(n.data, true);
+				previous = n;
+			}
+
+			n = n.next;
+		}
+	}
+
 	public static void main(String[] args) {
 
 		Node node = new Node(1);
@@ -52,6 +73,19 @@ class DeleteDuplicates{
 		System.out.println("*********** deleteDuplicateListsRecursive recursive*********** ");
 		node = deleteDuplicatesRecursive(node);
 		node.display();
+
+		Node node2 = new Node(9);
+		node2.appendToTail(6);
+		node2.appendToTail(3);
+		node2.appendToTail(4);
+		node2.appendToTail(9);
+		node2.appendToTail(8);
+		node2.appendToTail(3);
+
+
+		System.out.println("*********** deleteDuplicatesUnsortedList *********** ");
+		deleteDuplicatesUnsortedList(node2);
+		node2.display();
 		
 	}
 }
